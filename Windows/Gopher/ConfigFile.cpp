3c0c26bd18d1892ad14567e5eb10a6e924b34dd8
kamil <hamburgerxxl@o2.pl>
2021-06-25 06:35:29 +0200

    Revert "Merge pull request #83 from kkaja123/thumbstick_switch"
    This reverts commit 459588d3ce2000eac86c86a5ecbf1763e3a5dee1, reversing
    changes made to 1a3633a655fddcd2a92a43638413bed153a1db77.

--- Windows/Gopher/ConfigFile.cpp
+++ Windows/Gopher/ConfigFile.cpp
@@ -135,13 +135,10 @@ void ConfigFile::ExtractKeys()
     outfile << "GAMEPAD_TRIGGER_LEFT = 0" << std::endl;
     outfile << "GAMEPAD_TRIGGER_RIGHT = 0" << std::endl;
     outfile << "\n" << std::endl;
-    outfile << "# ADVANCED CONFIGURATION SETTINGS" << std::endl;
     outfile << "#  ALLOWED CURSOR SPEEDS, FIRST WILL BE CHOSEN BY DEFAULT.  VALUES > 1.0 WILL BE IGNORED.  NO SPACES." << std::endl;
     outfile << "CURSOR_SPEED = ULTRALOW=0.005,LOW=0.015,MED=0.025,HIGH=0.04" << std::endl;
     outfile << "#  SET ACCELERATION FACTOR FOR NON-LINEAR CURSOR SPEED" << std::endl;
     outfile << "# ACCELERATION_FACTOR = 3" << std::endl;
-    outfile << "#  Swaps the function of the thumbsticks. Set to 0 for default behavior or set to 1 to have the mouse movement on the right stick and scrolling on the left stick." << std::endl;
-    outfile << "SWAP_THUMBSTICKS = 0" << std::endl;
     // End config dump
 
     outfile.close();
